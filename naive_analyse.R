ff8676f6ea783ec4fa5304c6edf8101b3e992e1b
yashutanu <yashutanu1@gmail.com>
2018-05-15 22:30:25 +0530

    Delete naive_analyse.R

--- naive_analyse.R
+++
@@ -1,9 +0,0 @@
-#naive bayes model
-library('e1071')
-library('class')
-
-nb_model <- naiveBayes(Internship~Age+no_of_contest_win+hours_spent+algorithm+Data_structure+Resource
-                       +web+paper_presentation+research+back_end+app+data_science,data = codetrain)
-nb_model
-#model <- naiveBayes(Internship~, data = codetrain)
-predict(nb_model, codetrain[1:10,], type = "raw")
